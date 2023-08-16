6e93bf9d7f36f625ba9d0143e9a6fb6ebe5b23b7
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-08-16 18:18:10 +0100

    fix: the ui of the page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,15 @@
-
+import localFont from 'next/font/local'
 import "@/styles/global.scss";
 import Layout from "../Components/Layout";
-
+import {IBM_Plex_Sans} from "next/font/google"
+const ibmPlexSans = IBM_Plex_Sans({
+  weight: ['300','400', '700', '300', '500', '600'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <Layout className={ibmPlexSans.className}>
       <Component {...pageProps} />
     </Layout>
   );
