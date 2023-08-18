12caf01f352df648a24d7755c24b2960253c5476
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-08-18 13:38:58 +0100

    fix: google issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,12 +12,12 @@ export default function Document() {
         />
 
         <Script
+          
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
 
-        <Script 
-          strategy="lazyOnload">
+        <Script id="google-analytics-config" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
