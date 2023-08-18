d4747b315128da2761ecc3aed3d62173bc780fed
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-08-18 12:49:40 +0100

    add google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
+import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,6 +9,22 @@ export default function Document() {
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
           crossorigin="anonymous"
         />
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
       </Head>
       <body>
         <Main />
