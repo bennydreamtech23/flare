56da75675493f446b09e6f49af33635b37a96645
bennydreamtech23 <uwabunkeonyeijeoma@gmail.com>
2023-08-18 13:08:17 +0100

    fix: the ui of the page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,29 +1,32 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <Script
+          id="google-ads-script"
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
 
         <Script
+          id="google-analytics-script"
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
 
-        <Script strategy="lazyOnload">
+        <Script id="google-analytics-config" strategy="lazyOnload">
           {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `}
         </Script>
       </Head>
       <body>
